b228e69c669e525d79ab146d6b09d896c203296b
Andres <andrespedca@gmail.com>
2021-11-11 19:38:57 -0500

    cambio a mysql iniciado

--- src/empleado/empleado.controller.ts
+++ src/empleado/empleado.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateEmpleadoDTO } from './dto/create_empleado.dto';
+import { EmpleadoService } from './empleado.service';
 
 @Controller('empleado')
-export class EmpleadoController {}
+export class EmpleadoController {
+    constructor (private readonly empleadoService : EmpleadoService){}
+
+    @Get()
+    async getEmpleados (@Res() res)
+    {
+        const empleados = await this.empleadoService.getEmpleados();
+        return res.status(HttpStatus.OK).json(
+            {empleados : empleados}
+        );
+    }
+
+    @Post('/create')
+    async createEmpresa (@Res() res, @Body() crearEmpleadoDTO : CreateEmpleadoDTO)
+    {
+        const empleado = await this.empleadoService.crearEmpleado(crearEmpleadoDTO);
+
+        return res.status(HttpStatus.CREATED).json(
+            {
+                message: 'created',
+                empleado: empleado
+            }
+        );
+    }
+}
--- src/empleado/empleado.service.ts
+++ src/empleado/empleado.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Empresa } from 'src/empresa/schemas/empresa.schema';
 import { CreateEmpleadoDTO } from './dto/create_empleado.dto';
 import { Empleado, EmpleadoDocument } from './schema/empleado.schema';
 
 @Injectable()
 export class EmpleadoService {
-    constructor (@InjectModel(Empleado.name) private empleadoModel : Model<EmpleadoDocument>){}
+    constructor (@InjectModel(Empleado.name) private empleadoModel : Model<EmpleadoDocument>){
+        empleadoModel.findOne({nombre_empresa : 'Coolechera'}).populate({path : 'empresa_empleado'});
+    }
 
     async crearEmpleado (createEmpleadoDTO : CreateEmpleadoDTO) : Promise<Empleado>
     {
         const createdEmpleado = new this.empleadoModel(createEmpleadoDTO);
-        return createdEmpleado;
+        return createdEmpleado.save();
+    }
+
+    async getEmpleados (){
+        const empleado = await this.empleadoModel.find();
+        return empleado;
     }
 }
